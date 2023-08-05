93a5632da7e242754c05197d0297f26435150f24
Cesar Baez <baezcesar329@gmail.com>
2023-08-04 20:25:40 -0400

    Configuración inicial e instalación de Reactbooststrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
 
-import './App.css';
 import { Routes, Route } from "react-router-dom";
 import User from './components/User/User';
 import "bootstrap/dist/css/bootstrap.min.css"
 import 'bootstrap/dist/js/bootstrap.min.js'
+import './App.css';
+
 function App() {
   return (
-    
-      <Routes>
-        <Route exact path="/" element={<User/>} />
-      </Routes>
-   
+
+    <Routes>
+      <Route exact path="/" element={<User />} />
+    </Routes>
+
   );
 }
 
